b82b68358612504047aeb5bc28bab4439f3c199f
David Liu <david.yx.liu@oracle.com>
2022-10-06 12:06:04 +0800

    Update and rename git.md to git.sh

--- git.md
+++
@@ -1,3 +0,0 @@
-git push origin <tag_name>
-
-git push --delete origin <tag_name>
---
+++ git.sh
@@ -0,0 +1,17 @@
+push-tag(){
+  local tag_name = $1
+  git push origin $tag_name
+}
+delete-tag(){
+  local tag_name = $1
+  git push --delete origin $tag_name
+}
+setup(){
+  git config --global user.name "David Liu"
+  git config --global user.email "david-khala@hotmail.com"
+  git config pull.rebase true
+}
+$@
+
+
+
