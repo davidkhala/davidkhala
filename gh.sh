e17cf45d33834d810a20a41619f3011b5ed4165f
David Liu <david.yx.liu@oracle.com>
2022-11-10 21:20:03 +0800

    Update gh.sh

--- gh.sh
+++ gh.sh
@@ -6,4 +6,7 @@ rename-codespace() {
   gh codespace edit --codespace=$1 --display-name=$2
 
 }
+os-codespace() {
+  curl https://raw.githubusercontent.com/davidkhala/linux-utils/main/system.sh | bash -s get_distribution
+}
 $1
