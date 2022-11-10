fee1442883597cb94ab5e0505819c6c4c5c133e6
David Liu <david.yx.liu@oracle.com>
2022-11-10 21:20:38 +0800

    rollback
    this is not a `gh` command

--- gh.sh
+++ gh.sh
@@ -6,7 +6,5 @@ rename-codespace() {
   gh codespace edit --codespace=$1 --display-name=$2
 
 }
-os-codespace() {
-  curl https://raw.githubusercontent.com/davidkhala/linux-utils/main/system.sh | bash -s get_distribution
-}
+
 $1
