85b4154c07af9f93f625b593872d6cce44979fd6
David Liu <david.yx.liu@oracle.com>
2022-12-02 17:30:58 +0800

    add git: clone or update

--- git.sh
+++ git.sh
@@ -1,62 +1,50 @@
-push-tag(){
-  local tag_name = $1
-  git push origin $tag_name
-}
-delete-tag(){
-  local tag_name = $1
-  git push --delete origin $tag_name
-}
-setup(){
-  git config --global user.name "David Liu"
-  git config --global user.email "david-khala@hotmail.com"
-  git config --global pull.rebase true
-}
 set -e -x
+push-tag() {
+	local tag_name = $1
+	git push origin $tag_name
+}
+delete-tag() {
+	local tag_name = $1
+	git push --delete origin $tag_name
+}
+setup() {
+	git config --global user.name "David Liu"
+	git config --global user.email "david-khala@hotmail.com"
+	git config --global pull.rebase true
+}
 
-_clone() {
-	local GOPATH=$(go env GOPATH)
+# Git clone or update
+update() {
+	
 	local reposURL=$1
-	local orgName=$2
-	local projectName=$3
-	local branch=$4
-	local orgPath=${GOPATH}/src/github.com/${orgName}
-	mkdir -p "${orgPath}"
-	cd "${orgPath}"
-	if [[ ! -d ${orgPath}/${projectName} ]]; then
+	local branch=$2
+
+	local projectName
+
+	local dirname=${3:-$PWD}
+
+	if [[ ${reposURL} == github* ]]; then
+		clone-or-update "https://${reposURL}.git" $branch
+	elif [[ ${reposURL} == https://* ]]; then
+		projectName=$(echo ${reposURL} | cut -d '/' -f 5 | cut -d '.' -f 1)
+
+	elif [[ ${reposURL} == git@* ]]; then
+		echo ...using SSH
+		projectName=$(echo ${reposURL} | cut -d '/' -f 2 | cut -d '.' -f 1)
+	else
+		echo "Invalid git repository URL: ${reposURL}"
+		exit 1
+	fi
+
+	if [[ ! -d ${dirname}/${projectName} ]]; then
 		git clone "$reposURL"
 	fi
-	cd "${orgPath}/${projectName}"
+	cd "${dirname}/${projectName}"
 	git pull
 	if [[ -n $branch ]]; then
 		git checkout $branch
 	fi
-	echo "${orgPath}/${projectName}"
+	echo "${dirname}/${projectName}"
 }
 
-get() {
-	# support github and linux/unix only
-	local reposURL=$1
-	local branch=$2
-	local orgName
-	local projectName
-
-	if [[ ${reposURL} == github* ]]; then
-		get "https://${reposURL}.git" $branch
-	elif [[ ${reposURL} == https://* ]]; then
-		orgName=$(echo ${reposURL} | cut -d '/' -f 4)
-		projectName=$(echo ${reposURL} | cut -d '/' -f 5 | cut -d '.' -f 1)
-		_clone $reposURL $orgName $projectName $branch
-	elif [[ ${reposURL} == git@* ]]; then
-		echo ...using SSH
-		orgName=$(echo ${reposURL} | cut -d '/' -f 1 | cut -d ':' -f 2)
-		projectName=$(echo ${reposURL} | cut -d '/' -f 2 | cut -d '.' -f 1)
-
-		_clone $reposURL $orgName $projectName $branch
-	else
-		exit 1
-	fi
-}
 $@
-
-
-
